877dfc8a15a3fc881df52825b1cdef93ca33a3e9
daniil <kotik4491@gmail.com>
2024-05-30 19:14:35 +0300

    Скрипт запуска для линукс

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+export TIME_DIVISION_MS=$(awk '/^TIME_DIVISION_MS/{print $3}' .config)
+export TIME_MULTIPLICATION_M=$(awk '/^TIME_MULTIPLICATION_MS/{print $3}' .config)
+export TIME_SUBTRACTION_MS=$(awk '/^TIME_SUBTRACTION_MS/{print $3}' .config)
+export TIME_ADDITION_MS=$(awk '/^TIME_ADDITION_MS/{print $3}' .config)
+
+go build -C ./server/cmd  
+go build -C ./agent/cmd
+
+./server/cmd/cmd &
+./agent/cmd/cmd
+
+
+wait
